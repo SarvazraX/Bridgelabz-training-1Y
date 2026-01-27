d862be2e10f95d3e886effae16ee87b8d20a7e96
Raj Singh <singhrajk100@gmail.com>
2026-01-27 08:04:30 +0530

    Demo1

---
+++ Demo1.java
@@ -0,0 +1,16 @@
+public class Demo1{
+static int x;
+int y;
+public static void main(String[] args) {
+int p = 2 ;
+System.out.println("hello world");
+m1();
+
+}
+
+static void m1(){
+
+System.out.println("piyush is a getting smarter");
+
+}
+}
